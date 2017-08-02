b454a56401e48188b254d14e3a27032c11c611d6
garryk3 <egoruum@gmail.com>
2017-08-03 00:08:45 +0300

    start vue

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin">
+    <h3>Панель редактора</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style scoped>
+  .admin {
+
+  }
+</style>
